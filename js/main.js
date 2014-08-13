5acf36b90a3a0293d259ade8e3769490c809b29a
Kardos Robert <robert.kardos@replise.com>
2014-08-13 11:25:58 +0200

    Kalacs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-asdasdaasd
-asdsda
-asda
-asd
-asdasdasd
-=======
 var app = angular.module('myApp',[]);
 
 app.controller('ctrl',['$scope',function($scope){
   
 }]);
->>>>>>> newBranch
